c4e8ccc6a304a8b55003110263ea79be1f609601
城邦 <252389238@qq.com>
2022-07-30 11:36:56 +0800

    获取本机ip

--- ep/ip.go
+++ ep/ip.go
@@ -50,3 +50,21 @@ func Ipv4StringToInt(ip string) int64 {
 	ret.SetBytes(net.ParseIP(ip).To4())
 	return ret.Int64()
 }
+
+func GetLoaclIp() string {
+	var err error
+	var ip = "127.0.0.1"
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return ip
+	}
+	for _, address := range addrs {
+		// 检查ip地址判断是否回环地址
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String()
+			}
+		}
+	}
+	return ip
+}
