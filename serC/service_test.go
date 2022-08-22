55990d1fd23eb8dc39f1d76feda324e3ef5b47e0
jiajunxi <252389238@qq.com>
2022-08-22 19:11:08 +0800

    no message

--- serC/service_test.go
+++ serC/service_test.go
@@ -5,12 +5,13 @@ import (
 	"fmt"
 	clientv3 "go.etcd.io/etcd/client/v3"
 	"log"
+	"strconv"
 	"testing"
 	"time"
 )
 
 func TestNew(t *testing.T) {
-	ctx, cancel := context.WithCancel(context.Background())
+	ctx, _ := context.WithCancel(context.Background())
 	etcds := []string{"127.0.0.1:2379"}
 	c, err := New(etcds, "/test-01", "127.0.0.1", 5, ctx)
 	fmt.Println(c.WatchService("/test"))
@@ -24,31 +25,39 @@ func TestNew(t *testing.T) {
 		}
 	}()
 	go func() {
-		time.Sleep(time.Second * 3)
-		cli, err := clientv3.New(clientv3.Config{
-			Endpoints:   etcds,
-			DialTimeout: 5 * time.Second,
-		})
-		if err != nil {
-			log.Fatal(err)
-		}
-		resp, err := cli.Grant(context.Background(), 5)
-		if err != nil {
-			log.Fatal(err)
-		}
-		//注册服务并绑定租约
-		_, err = cli.Put(context.Background(), "/test-02", "127.0.0.2", clientv3.WithLease(resp.ID))
-		if err != nil {
-			log.Fatal(err)
+		for i := 0; i < 10; i++ {
+			addService(i)
+			time.Sleep(time.Second * 1)
 		}
 	}()
 	i := 0
 	for {
-		if i == 10 {
+		/*if i == 10 {
 			cancel()
-		}
+		}*/
 		fmt.Println("所有服务器:", c.GetServices())
 		time.Sleep(time.Second * 1)
 		i++
+	}
+}
+
+func addService(i int) {
+	etcds := []string{"127.0.0.1:2379"}
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints:   etcds,
+		DialTimeout: 5 * time.Second,
+	})
+	defer cli.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	resp, err := cli.Grant(context.Background(), 20)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//注册服务并绑定租约
+	_, err = cli.Put(context.Background(), "/test-0"+strconv.Itoa(i+1), "127.0.0."+strconv.Itoa(i), clientv3.WithLease(resp.ID))
+	if err != nil {
+		log.Fatal(err)
 	}
 }
