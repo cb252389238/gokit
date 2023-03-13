4214e3cfd253b912e7c1dd21e8f32d8bd058c42d
jiajunxi <252389238@qq.com>
2023-03-13 11:21:31 +0800

    提交变更

--- ori/typedef/common.go
+++ ori/typedef/common.go
@@ -1,17 +1,12 @@
 package typedef
 
 type Request struct {
-	Data ReqData `json:"data"` //数据内容
-}
-
-type ReqData struct {
-	Msg string `json:"text" binding:"required,max=1024"` //检测文本
+	Data any `json:"data"` //数据内容
 }
 
 // 响应结构体
 type Response struct {
 	Code      int    `json:"code"`      //返回码 0成功 http相关返回码1000-1999 tcp相关返回码2000-2999 服务相关错误码3000-
-	Message   string `json:"message"`   //返回描述
 	RequestId string `json:"requestId"` //唯一请求ID
 	Msg       string `json:"msg"`
 }
