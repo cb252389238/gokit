c2cda7473493b9dcf47349da39d55beba011fcfc
jiajunxi <252389238@qq.com>
2023-04-25 13:55:43 +0800

    添加加密解密函数

---
+++ ori/core/oriTools/easy/encoding.go
@@ -0,0 +1,86 @@
+package easy
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/ianaindex"
+	"golang.org/x/text/transform"
+	"io"
+	"io/ioutil"
+)
+
+var (
+	// Alias for charsets.
+	charsetAlias = map[string]string{
+		"HZGB2312": "HZ-GB-2312",
+		"hzgb2312": "HZ-GB-2312",
+		"GB2312":   "HZ-GB-2312",
+		"gb2312":   "HZ-GB-2312",
+	}
+)
+
+// 检查是否支持
+func Supported(charset string) bool {
+	return getEncoding(charset) != nil
+}
+
+// 转换字符编码
+// dstCharset 目标编码   srcCharset 源编码
+func Convert(dstCharset string, srcCharset string, src string) (dst string, err error) {
+	if dstCharset == srcCharset {
+		return src, nil
+	}
+	dst = src
+	if srcCharset != "UTF-8" {
+		if e := getEncoding(srcCharset); e != nil {
+			tmp, err := io.ReadAll(
+				transform.NewReader(bytes.NewReader([]byte(src)), e.NewDecoder()),
+			)
+			if err != nil {
+				return "", err
+			}
+			src = string(tmp)
+		} else {
+			return dst, errors.New(fmt.Sprintf("unsupported srcCharset: %s", srcCharset))
+		}
+	}
+	if dstCharset != "UTF-8" {
+		if e := getEncoding(dstCharset); e != nil {
+			tmp, err := ioutil.ReadAll(
+				transform.NewReader(bytes.NewReader([]byte(src)), e.NewEncoder()),
+			)
+			if err != nil {
+				return "", errors.New(fmt.Sprintf("unsupported srcCharset: %s", dstCharset))
+			}
+			dst = string(tmp)
+		} else {
+			return dst, errors.New(fmt.Sprintf("unsupported srcCharset: %s", dstCharset))
+		}
+	} else {
+		dst = src
+	}
+	return dst, nil
+}
+
+// 字符转utf8
+// srcCharset 源编码 src 需要转的字符串
+func ToUTF8(srcCharset string, src string) (dst string, err error) {
+	return Convert("UTF-8", srcCharset, src)
+}
+
+// utf8转其他编码
+func UTF8To(dstCharset string, src string) (dst string, err error) {
+	return Convert(dstCharset, "UTF-8", src)
+}
+
+func getEncoding(charset string) encoding.Encoding {
+	if c, ok := charsetAlias[charset]; ok {
+		charset = c
+	}
+	if e, err := ianaindex.MIB.Encoding(charset); err == nil && e != nil {
+		return e
+	}
+	return nil
+}
---
+++ ori/core/oriTools/easy/encrypt_test.go
@@ -0,0 +1,37 @@
+package easy
+
+import (
+	"fmt"
+	"testing"
+)
+
+var (
+	privateKey = `-----BEGIN RSA PRIVATE KEY-----
+MIIBPAIBAAJBAKOFqluzvKBxVWdYi2fNQCYPWJ7sC0mqH0WLrsnCTmSWDZMhm6pp
+IMxzV6OElphDGi/hxgQBg9dwn8xDkqJzqwcCAwEAAQJBAIwZGATrMB+yGf6qEP4F
+DwHMwhuelmktlQ9Lhpwbmnh37Dw8Vp5yKAEqpTtmYD0KG7QzlU5cYaTtFioS6A2l
+OiECIQDSerkA2Doi3FbjlrhIsWR88EneyS2NUaq+twh2VeZn9wIhAMbjIpobMCS1
+b0C+d3Bum399C/1hDfdiAtUCWbwZkrFxAiBg64X5L5hFqTSRhDvDrXvaVEOPxQ+m
+vW5kd5/77b41LQIhAJgLCefPwxU9EsjnEr4EAKIMwX65lIi7B7k5q8oNrsQxAiEA
+0Tx3+3BFtT5cFZroNn85W8iqxd4p82Dh31IuRasNvt4=
+-----END RSA PRIVATE KEY-----`
+	publicKey = `-----BEGIN PUBLIC KEY-----
+MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAKOFqluzvKBxVWdYi2fNQCYPWJ7sC0mq
+H0WLrsnCTmSWDZMhm6ppIMxzV6OElphDGi/hxgQBg9dwn8xDkqJzqwcCAwEAAQ==
+-----END PUBLIC KEY-----`
+)
+
+func TestGenerateRSAKey(t *testing.T) {
+	rsa := NewRsa("", "")
+	key, publicKey := rsa.CreateKeys(512)
+	fmt.Println(key)
+	fmt.Println(publicKey)
+}
+
+func TestRsa_Encrypt(t *testing.T) {
+	rsa := NewRsa(publicKey, privateKey)
+	encrypt, err := rsa.Encrypt([]byte("加密测试消息"))
+	fmt.Println(encrypt, err)
+	decrypt, err := rsa.Decrypt(encrypt)
+	fmt.Println(string(decrypt), err)
+}
