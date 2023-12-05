8296b5f392e8b0a343f92abb8ae5dd2e7cce22c5
jiajunxi <jiajunx@nextmar.com>
2023-12-05 11:45:31 +0800

    测试文档修改

--- goAuth/auth_test.go
+++ goAuth/auth_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestNew(t *testing.T) {
-	goAuth, err := New("root:root@tcp(127.0.0.1:3306)/中文情感分析?charset=utf8")
+	goAuth, err := New("root:root@tcp(127.0.0.1:3306)/database?charset=utf8")
 	if err != nil {
 		panic(err)
 	}
