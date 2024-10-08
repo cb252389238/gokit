7ece6c2944ec2a78cd5d693583984398eaf6c9c0
jiajunxi <252389238@qq.com>
2024-10-08 08:59:06 +0800

    增加脱敏函数，对手机号 身份证号脱敏

--- ori/core/oriTools/easy/string.go
+++ ori/core/oriTools/easy/string.go
@@ -2,11 +2,42 @@ package easy
 
 import "strings"
 
-// 对手机号进行隐藏
+// 脱敏手机号
 func PrivateMobile(mobile string) string {
 	masked := ""
 	if len(mobile) > 5 {
 		masked = mobile[:3] + strings.Repeat("*", len(mobile)-5) + mobile[len(mobile)-2:]
+	}
+	return masked
+}
+
+// 脱敏名字
+func PrivateRealName(name string) string {
+	masked := ""
+	runeName := []rune(name)
+	switch {
+	case len(runeName) > 3:
+		masked = string(runeName[0]) + strings.Repeat("*", len(runeName)-2) + string(runeName[len(runeName)-1:])
+	case len(runeName) == 3:
+		masked = string(runeName[0]) + strings.Repeat("*", 1) + string(runeName[len(runeName)-1:])
+	case len(runeName) == 2:
+		masked = string(runeName[0]) + strings.Repeat("*", 1)
+	default:
+		masked = name
+	}
+	return masked
+}
+
+// 脱敏身份证号
+func PrivateIdNo(idNo string) string {
+	masked := ""
+	switch {
+	case len(idNo) > 14:
+		masked = idNo[0:4] + strings.Repeat("*", len(idNo)-2) + idNo[len(idNo)-2:]
+	case len(idNo) < 6:
+		masked = idNo[0:2] + strings.Repeat("*", len(idNo)-2) + idNo[len(idNo)-2:]
+	default:
+		masked = idNo[0:4] + strings.Repeat("*", len(idNo)-2) + idNo[len(idNo)-2:]
 	}
 	return masked
 }
