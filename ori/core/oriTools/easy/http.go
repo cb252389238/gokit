eb8db2aed19c5457ed20c42a459cc92509039e53
jiajunxi <252389238@qq.com>
2024-11-05 14:33:57 +0800

    增加post请求

--- ori/core/oriTools/easy/http.go
+++ ori/core/oriTools/easy/http.go
@@ -3,7 +3,9 @@ package easy
 import (
 	"bytes"
 	"io"
+	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 func Get(urlStr string) (string, error) {
@@ -32,4 +34,31 @@ func Post(url string, data []byte) (string, error) {
 	}
 	defer resp.Body.Close()
 	return string(body), nil
+}
+
+func HttpDo(httpMethod string, url string, headerMap map[string]string, rawBody string) (remoteResponse string, err error) {
+	client := &http.Client{}
+	req, err0 := http.NewRequest(httpMethod, url, strings.NewReader(rawBody))
+	if err0 != nil {
+		err = err0
+		return
+	}
+	if len(headerMap) > 0 {
+		for k, v := range headerMap {
+			req.Header.Set(k, v)
+		}
+	}
+	resp, err1 := client.Do(req)
+	if err1 != nil {
+		err = err1
+		return
+	}
+	defer resp.Body.Close()
+	body, err2 := ioutil.ReadAll(resp.Body)
+	if err2 != nil {
+		err = err2
+		return
+	}
+	remoteResponse = string(body)
+	return
 }
