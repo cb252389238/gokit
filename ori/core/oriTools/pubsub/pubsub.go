87d253bdcb6214d41f1f0b9dd7216d1487fc2f39
jiajunxi <252389238@qq.com>
2024-11-09 16:32:40 +0800

    晚上发布订阅功能组件，增加客户端区分，消息补偿

--- ori/core/oriTools/pubsub/pubsub.go
+++ ori/core/oriTools/pubsub/pubsub.go
@@ -2,140 +2,281 @@
 package pubsub
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
+	"reflect"
+	"strings"
 	"sync"
+	"time"
 )
 
-const (
-	DEFAULT_TOPIC = "default_topic"
-)
+var publisher *Publisher
+var publisherOnce sync.Once
 
 type topicType = string
 type uidType = string
-type keyType = int64
+type platformType = string //客户端类型
 
 // 发布者
 type Publisher struct {
-	m           sync.RWMutex                          //读写锁
-	buffer      int                                   //订阅队列缓存大小
-	subscribers map[topicType]map[keyType]*Subscriber //主题订阅者集合
-	users       map[uidType]*Subscriber               //用户和订阅实例绑定关系
-	snowflake   *Node
+	m             sync.RWMutex                          //读写锁
+	buffer        int                                   //订阅队列缓存大小
+	subscribers   map[topicType]map[uidType]*Subscriber //主题订阅者集合
+	users         map[uidType]*Subscriber               //用户和订阅实例绑定关系
+	snowflake     *Node
+	snLen         int64 //详细补偿队列
+	platformArray []string
 }
 
+type snMsg struct {
+	Sn         int64 `json:"sn"` //消息序列号
+	Msg        any   `json:"msg"`
+	createTime int64 `json:"-"`
+}
+
+// 订阅者
 type Subscriber struct {
-	C     chan any
-	Key   keyType
-	Topic map[topicType]struct{} //订阅得主题
-	uid   uidType
+	C        chan any
+	Topic    map[topicType]struct{} //订阅得主题
+	uid      uidType                //用户标识
+	store    []snMsg                //消息缓存
+	sn       int64                  //消息序列号
+	platform platformType           //客户端类型
 }
 
 // 构建一个新的发布者对象
-func New() *Publisher {
-	node, _ := NewNode(int64(1))
-	return &Publisher{
-		m:           sync.RWMutex{},
-		buffer:      10,
-		subscribers: make(map[topicType]map[keyType]*Subscriber),
-		users:       make(map[uidType]*Subscriber),
-		snowflake:   node,
+func New(platform []string) (*Publisher, error) {
+	if len(platform) == 0 {
+		return nil, errors.New("platform is empty")
 	}
+	publisherOnce.Do(func() {
+		node, _ := NewNode(int64(1))
+		publisher = &Publisher{
+			m:             sync.RWMutex{},
+			buffer:        100,
+			subscribers:   make(map[topicType]map[uidType]*Subscriber),
+			users:         make(map[uidType]*Subscriber),
+			snowflake:     node,
+			snLen:         1000,
+			platformArray: platform,
+		}
+	})
+	return publisher, nil
 }
 
-// 订阅一个主题
-func (p *Publisher) Subscribe(topic, uid string) (*Subscriber, error) {
+func getUserId(uid, platform string) string {
+	return fmt.Sprintf("%s:%s", uid, platform)
+}
+
+func inArray(needle any, haystack any) bool {
+	val := reflect.ValueOf(haystack)
+	switch val.Kind() {
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < val.Len(); i++ {
+			if reflect.DeepEqual(needle, val.Index(i).Interface()) {
+				return true
+			}
+		}
+	case reflect.Map:
+		for _, k := range val.MapKeys() {
+			if reflect.DeepEqual(needle, val.MapIndex(k).Interface()) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+// 订阅主题 topic主题 userId 用户id platform客户端类型
+func (p *Publisher) Subscribe(topic, uid, platform string) (*Subscriber, error) {
+	p.m.Lock()
+	defer p.m.Unlock()
 	if uid == "" {
 		return nil, errors.New("uid is empty")
 	}
-	if topic == "" {
-		topic = DEFAULT_TOPIC
+	if platform == "" {
+		return nil, errors.New("platform is empty")
 	}
-	p.m.Lock()
-	defer p.m.Unlock()
+	if topic == "" {
+		return nil, errors.New("topic is empty")
+	}
+	if !inArray(platform, p.platformArray) {
+		return nil, errors.New("platform is not exist")
+	}
 	var sub *Subscriber
-
+	userId := getUserId(uid, platform)
 	//用户是否已经创建订阅实体
-	if val, ok := p.users[uid]; ok {
+	if val, ok := p.users[userId]; ok {
 		sub = val
 		sub.Topic[topic] = struct{}{}
 	} else {
-		key := p.snowflake.Generate().Int64()
 		sub = &Subscriber{
-			C:     make(chan any, p.buffer), //消息接受通道
-			Topic: map[topicType]struct{}{topic: {}},
-			uid:   uid,
-			Key:   key,
+			C:        make(chan any, p.buffer), //消息接受通道
+			Topic:    map[topicType]struct{}{topic: {}},
+			uid:      userId,
+			store:    make([]snMsg, p.snLen),
+			platform: platform,
 		}
 	}
-
 	if subSets, ok := p.subscribers[topic]; ok {
-		if s, ok := subSets[sub.Key]; ok {
+		if s, ok := subSets[sub.uid]; ok {
 			s.Topic[topic] = struct{}{}
 		} else {
-			subSets[sub.Key] = sub
+			subSets[sub.uid] = sub
 		}
 	} else {
-		subSets = map[int64]*Subscriber{
-			sub.Key: sub,
+		subSets = map[uidType]*Subscriber{
+			sub.uid: sub,
 		}
 		p.subscribers[topic] = subSets
 	}
-	p.users[uid] = sub
+	p.users[userId] = sub
 	return sub, nil
 }
 
 // 退出订阅
-func (p *Publisher) Exit(sub *Subscriber, topic string) {
-	if sub == nil {
-		return
+func (p *Publisher) Exit(topic, uid, platform string) error {
+	if uid == "" {
+		return errors.New("uid is empty")
 	}
+	if platform == "" {
+		return errors.New("platform is empty")
+	}
+	if topic == "" {
+		return errors.New("topic is empty")
+	}
+	if !inArray(platform, p.platformArray) {
+		return errors.New("platform is not exist")
+	}
+	userId := getUserId(uid, platform)
 	p.m.Lock()
 	defer p.m.Unlock()
 	if subSets, ok := p.subscribers[topic]; ok {
-		delete(subSets, sub.Key)
+		delete(subSets, userId)
 	}
-	if user, ok := p.users[sub.uid]; ok {
+	if user, ok := p.users[userId]; ok {
 		delete(user.Topic, topic)
 	}
+	return nil
 }
 
 // 发布一个主题信息
-func (p *Publisher) Publish(topic string, message string) {
+func (p *Publisher) Publish(topic string, message any, platform ...string) {
 	p.m.RLock()
 	defer p.m.RUnlock()
 	if subSets, ok := p.subscribers[topic]; ok {
 		for _, v := range subSets {
-			go func(v *Subscriber) {
-				select {
-				case v.C <- message:
-				default:
-					//忽略数据
-				}
-			}(v)
+			split := strings.Split(v.uid, ":")
+			if len(split) != 2 {
+				continue
+			}
+			if len(platform) > 0 && !inArray(split[1], platform) {
+				continue
+			}
+			offset := v.sn % p.snLen
+			buildMsg := snMsg{
+				Sn:         v.sn,
+				Msg:        message,
+				createTime: time.Now().Unix(),
+			}
+			v.store[offset] = buildMsg
+			buildMsgByte, _ := json.Marshal(buildMsg)
+			sub := v
+			select {
+			case sub.C <- string(buildMsgByte):
+			default:
+				//忽略数据
+			}
+			v.sn++
 		}
 	}
 }
 
-// 向所有房间推送主题
-func (p *Publisher) PublishAll(message any) {
+// 向所有用户推送主题
+func (p *Publisher) PublishAll(message any, platform ...string) {
 	p.m.RLock()
 	defer p.m.RUnlock()
-	for _, sub := range p.users {
-		go func(v *Subscriber) {
-			select {
-			case v.C <- message:
-			default:
-				//忽略数据
-			}
-		}(sub)
+	for _, v := range p.users {
+		split := strings.Split(v.uid, ":")
+		if len(split) != 2 {
+			continue
+		}
+		if len(platform) > 0 && !inArray(split[1], platform) {
+			continue
+		}
+		offset := v.sn % p.snLen
+		buildMsg := snMsg{
+			Sn:         v.sn,
+			Msg:        message,
+			createTime: time.Now().Unix(),
+		}
+		v.store[offset] = buildMsg
+		buildMsgByte, _ := json.Marshal(buildMsg)
+		sub := v
+		select {
+		case sub.C <- string(buildMsgByte):
+		default:
+			//忽略数据
+		}
+		v.sn++
 	}
 }
 
 // 向单个用户发送主题
-func (p *Publisher) PublishToUser(uid string, message any) {
+func (p *Publisher) PublishToUser(uid string, message any, platform ...string) {
 	p.m.RLock()
 	defer p.m.RUnlock()
-	if sub, ok := p.users[uid]; ok {
-		sub.C <- message
+	if len(platform) > 0 {
+		for _, plat := range platform {
+			if !inArray(plat, p.platformArray) {
+				continue
+			}
+			userId := getUserId(uid, plat)
+			if sub, ok := p.users[userId]; ok {
+				offset := sub.sn % p.snLen
+				buildMsg := snMsg{
+					Sn:         sub.sn,
+					Msg:        message,
+					createTime: time.Now().Unix(),
+				}
+				sub.store[offset] = buildMsg
+				buildMsgByte, _ := json.Marshal(buildMsg)
+				sub.C <- string(buildMsgByte)
+				sub.sn++
+			}
+		}
+	} else {
+		for _, plat := range p.platformArray {
+			userId := getUserId(uid, plat)
+			if sub, ok := p.users[userId]; ok {
+				offset := sub.sn % p.snLen
+				buildMsg := snMsg{
+					Sn:         sub.sn,
+					Msg:        message,
+					createTime: time.Now().Unix(),
+				}
+				sub.store[offset] = buildMsg
+				buildMsgByte, _ := json.Marshal(buildMsg)
+				sub.C <- string(buildMsgByte)
+				sub.sn++
+			}
+		}
+	}
+}
+
+// 发送补偿消息
+func (p *Publisher) Replier(uid, platform string, sn int64) {
+	p.m.RLock()
+	defer p.m.RUnlock()
+	userId := getUserId(uid, platform)
+	if sub, ok := p.users[userId]; ok {
+		offset := sn % p.snLen
+		buildMsg := sub.store[offset]
+		if time.Now().Unix()-buildMsg.createTime > 60 {
+			return
+		}
+		buildMsgByte, _ := json.Marshal(buildMsg)
+		sub.C <- string(buildMsgByte)
 	}
 }
--- ori/core/oriTools/pubsub/pubsub_test.go
+++ ori/core/oriTools/pubsub/pubsub_test.go
@@ -2,53 +2,70 @@ package pubsub
 
 import (
 	"fmt"
-	"strconv"
 	"testing"
 	"time"
 )
 
 func TestPubsub(t *testing.T) {
 	//初始化一个发布者对象
-	pub := New()
-	user1Sub, _ := pub.Subscribe("topic1", "1001")
-	user1Sub, _ = pub.Subscribe("topic2", "1001")
-	user2Sub, _ := pub.Subscribe("topic1", "1002")
+	platformArr := []string{"pc", "h5", "app"}
+	pub, err := New(platformArr)
+	if err != nil {
+		t.Fatal(err)
+	}
+	user1_pc, _ := pub.Subscribe("topic1", "1001", "pc")
+	user1_app, _ := pub.Subscribe("topic1", "1001", "app")
+	user2_app, _ := pub.Subscribe("topic1", "1002", "app")
+	user3_app, _ := pub.Subscribe("topic2", "1003", "app")
+	user4_app, _ := pub.Subscribe("topic2", "1004", "app")
 
 	go func() {
-		i := 0
-		for {
-			pub.Publish("topic1", "topic1测试消息"+strconv.Itoa(i))
-			i++
-			time.Sleep(time.Second)
+		for message := range user1_pc.C {
+			fmt.Println("1001-pc:" + message.(string))
 		}
 	}()
 	go func() {
-		i := 0
-		for {
-			pub.Publish("topic2", "topic2测试消息"+strconv.Itoa(i))
-			i++
-			time.Sleep(time.Second)
+		for message := range user1_app.C {
+			fmt.Println("1001-app:" + message.(string))
 		}
 	}()
 	go func() {
-		for message := range user1Sub.C {
-			fmt.Println("user1-" + message.(string))
+		for message := range user2_app.C {
+			fmt.Println("1002-app:" + message.(string))
 		}
 	}()
 	go func() {
-		for message := range user2Sub.C {
-			fmt.Println("user2-" + message.(string))
+		for message := range user3_app.C {
+			fmt.Println("1003-app:" + message.(string))
 		}
 	}()
-	pub.PublishToUser("1001", "单独推送给用户的消息")
+	go func() {
+		for message := range user4_app.C {
+			fmt.Println("1004-app:" + message.(string))
+		}
+	}()
+	time.Sleep(time.Second)
+	fmt.Println("向topic1推送消息，1001-pc,1001-app，1002-app收到消息")
+	pub.Publish("topic1", "推送给订阅topic1的用户的消息")
+	time.Sleep(time.Second)
+	fmt.Println("向topic2推送消息，1003-app，1004-app收到消息")
+	pub.Publish("topic2", "推送给订阅topic2的用户的消息")
+	time.Sleep(time.Second)
+	fmt.Println("向所有用户推送消息 1001-pc,1001-app，1002-app，1003-app，1004-app收到消息")
 	pub.PublishAll("推送给所有用户的消息")
-	for {
-		select {
-		case <-time.After(time.Second * 10):
-			pub.Exit(user2Sub, "topic1")
-			pub.Exit(user1Sub, "topic1")
-			time.Sleep(time.Second * 5)
-			return
-		}
-	}
+	time.Sleep(time.Second)
+	fmt.Println("向1001推送消息 1001-pc,1001-app收到")
+	pub.PublishToUser("1001", "推送给1001用户的消息")
+	time.Sleep(time.Second)
+	fmt.Println("向1001app推送消息 1001-app收到")
+	pub.PublishToUser("1001", "推送给1001-app用户的消息", "app")
+	time.Sleep(time.Second)
+	fmt.Println("向1001pc推送消息 1001-pc收到")
+	pub.PublishToUser("1001", "推送给1001-pc用户的消息", "pc")
+	time.Sleep(time.Second)
+	fmt.Println("1001-pc退出订阅")
+	pub.Exit("topic1", "1001", "pc")
+	time.Sleep(time.Second)
+	fmt.Println("向topic1推送消息，1001-app，1002-app收到消息")
+	pub.Publish("topic1", "推送给订阅topic1的用户的消息")
 }
